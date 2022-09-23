6cf7595bb22bc5bc9eacf03bbcb042b2b52bf98b
coalpha <22552895+coalpha@users.noreply.github.com>
2022-09-23 11:49:04 -0700

    scunge

--- green.c
+++ green.c
@@ -5,11 +5,12 @@
 
 size_t const sig = *L"\xc0\xa1\xff\x1e";
 
-static char const prompt[] = "Generate a stub!\n> ";
+static char const prompt[] = "Generate a stub!\nTruncates to 260 characters!\n> ";
 
 void start(void) {
    unsigned char *pos = bin_shrub_exe;
    while (1) {
+      // yes, unaligned memory reads
       size_t const next_8 = *((size_t *) pos);
       if (next_8 == sig) break;
       pos++;
@@ -18,18 +19,12 @@ void start(void) {
    HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
    DWORD dummy;
    WriteConsoleA(hStdout, prompt, sizeof(prompt) - 1, &dummy, NULL);
-   CONSOLE_SCREEN_BUFFER_INFO buffer_info = {0};
-   if (GetConsoleScreenBufferInfo(hStdout, &buffer_info) != FALSE) {
-      ExitProcess(GetLastError());
-   }
-   DWORD input_length = 0
-      - (buffer_info.dwSize.X * (buffer_info.dwCursorPosition.Y + 1))
-      - (buffer_info.dwSize.X - (buffer_info.dwCursorPosition.X + 0));
-   LPWSTR input = __builtin_alloca(input_length * sizeof(WCHAR));
+   LPWSTR input = __builtin_alloca(260 * sizeof(WCHAR));
    HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-   ReadConsoleW(hStdin, input, input_length, &dummy, NULL);
-   WCHAR *next_pos = (WCHAR *) pos;
-   while (input_length --> 0) {
+   DWORD chars_left;
+   ReadConsoleW(hStdin, input, 260, &chars_left, NULL);
+   WCHAR *next_pos = (WCHAR *) pos; // also probably unaligned memory reads
+   while (chars_left --> 0) {
       *next_pos++ = *input++;
    }
    *next_pos = L'\0';
@@ -43,4 +38,5 @@ void start(void) {
       NULL
    );
    WriteFile(hStub, bin_shrub_exe, bin_shrub_exe_len, &dummy, NULL);
+   ExitProcess(0);
 }
--- shrub.c
+++ shrub.c
@@ -1,10 +1,13 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-static WCHAR EXE_PATH[MAX_PATH] =
-L"\xc0\xa1\xff\x1e*************************************************************************************************************************************************************************************************************************************************************";
+// static WCHAR EXE_PATH[MAX_PATH] =
+// L"\xc0\xa1\xff\x1e*************************************************************************************************************************************************************************************************************************************************************";
+
+static WCHAR EXE_PATH[] = L"C:\\Windows\\cmd.exe";
 
 void start(void) {
+   // need to ignore first argument in GetCommandLineW.
    STARTUPINFOW startupinfo;
    GetStartupInfoW(&startupinfo);
    size_t const current_directory_sz = GetCurrentDirectoryW(0, NULL); // in characters
@@ -19,7 +22,7 @@ void start(void) {
       0,                 // dwCreationFlags (inherit)
       NULL,              // use parent process's environment block
       current_directory, // lpCurrentDirectory
-      &startupinfo,       // lpStartupInfo
+      &startupinfo,      // lpStartupInfo
       &new_proc_info     // lpProcessInformation [out]
    );
 }
